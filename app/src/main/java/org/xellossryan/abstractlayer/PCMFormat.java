3912352ac15ae9a58df518e522b6d2a0e1e0455d
GinRyan <lp8800lz@gmail.com>
2017-05-03 11:34:13 +0800

    add pcm

---
+++ app/src/main/java/org/xellossryan/abstractlayer/PCMFormat.java
@@ -0,0 +1,27 @@
+package org.xellossryan.abstractlayer;
+
+import android.media.AudioFormat;
+
+/**
+ * 枚举PCM采样位率
+ */
+public enum PCMFormat {
+    PCM_8BIT(1, AudioFormat.ENCODING_PCM_8BIT),
+    PCM_16BIT(2, AudioFormat.ENCODING_PCM_16BIT);
+
+    private int bytesPerFrame;
+    private int audioFormat;
+
+    PCMFormat(int bytesPerFrame, int audioFormat) {
+        this.bytesPerFrame = bytesPerFrame;
+        this.audioFormat = audioFormat;
+    }
+
+    public int getBytesPerFrame() {
+        return bytesPerFrame;
+    }
+
+    public int getAudioFormat() {
+        return audioFormat;
+    }
+}
